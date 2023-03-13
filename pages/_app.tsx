1f5d12cc9b8dbb07081c7628f6cb04e03557e70f
arthur leriche <lerichearthur@gmail.com>
2023-03-13 15:21:03 +0100

    remove .env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-import { useState } from 'react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import type { AppProps } from 'next/app';
 
 function HiringApp({
   Component,
   pageProps,
 }: AppProps<{
-  initialSession: Session,
+  initialSession: Session;
 }>) {
   // Create a new supabase client for each page load
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
     // Provide the supabase client and initial session to the session context
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
     </SessionContextProvider>
-  )
+  );
 }
-export default HiringApp
+export default HiringApp;
